818c61df8995067628b99848a169f2c84fb58b84
Vansh1252 <sanklechav65@gmail.com>
2025-01-27 14:05:17 +0530

    add user model and video model

---
+++ src/models/video.models.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const mongooseAggregatePaginate = require('mongoose-aggregate-paginate');
+
+const videoschema = new mongoose.Schema({
+    videofile: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: true
+    }
+
+
+}, { timestamps: true });
+
+
+videoschema.plugin(mongooseAggregatePaginate)
+
+
+const videomodel = mongoose.model('video', videoschema);
+
+module.exports = videomodel;
