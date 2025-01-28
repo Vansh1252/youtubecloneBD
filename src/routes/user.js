1a7c0c2537bd609249f05a4e761cd9ed12d8babd
Vansh1252 <sanklechav65@gmail.com>
2025-01-28 17:16:10 +0530

    final user push

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registeruser, loginuser, logoutuser, refreshAccessToken } = require('../controllers/user');
+const { registeruser, loginuser, logoutuser, refreshAccessToken, avatarupdateuser, changecurrectpassword, coverImageupdateuser, getcurrentuser, updateAccountdetails } = require('../controllers/user');
 const router = express.Router();
 const upload = require('../middlewares/multermiddlewares');
 const verifyjwt = require('../middlewares/authmiddlewares');
@@ -16,6 +16,14 @@ router.post('/register', upload.fields([
 ]), registeruser);
 router.post('/loginIn', loginuser);
 router.post('/logout', verifyjwt, logoutuser);
-router.post('/refresh-token', refreshAccessToken)
+router.post('/refresh-token', refreshAccessToken);
+router.post('/updateavatar', avatarupdateuser);
+router.post('/changepassword', changecurrectpassword);
+router.post('/updatecoverImage', coverImageupdateuser);
+router.get('/getcurrentuser', getcurrentuser);
+router.post('/updateaccount', updateAccountdetails);
+
+
+
 
 module.exports = router;
