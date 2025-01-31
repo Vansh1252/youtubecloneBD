bb1d85af7c3a1c9db791db5e5becc06e93ed2bd0
Vansh1252 <sanklechav65@gmail.com>
2025-01-31 14:54:38 +0530

    some small bug fix in comment

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const verifyjwt = require('../middlewares/authmiddlewares');
+const { deletecomment, getvideocomment, save } = require('../controllers/comments');
 
 
-
+router.post('/save', verifyjwt, save);
+router.get('/', verifyjwt, getvideocomment);
+router.get('/delete', verifyjwt, deletecomment);
 
 module.exports = router;
