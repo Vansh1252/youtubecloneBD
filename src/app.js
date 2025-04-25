012f239257e0784bca421b62020f5a0fa9aa75a8
Vansh1252 <sanklechav65@gmail.com>
2025-04-25 16:21:08 +0530

    some changes in this things

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const path =require('path')
 const cookieParser = require('cookie-parser');
 const app = express();
+const logger = require('morgan');
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -13,6 +13,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(logger('dev'));
 
 
 const userroutes = require('./routes/user.js');
--- src/routes/videos.js
+++ src/routes/videos.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { getallvideos, publishAVideo, deleteVideo, getVideoById, togglePublishStatus, updateVideo } = require('../controllers/videos');
 const upload = require('../middlewares/multermiddlewares');
 const verifyjwt = require('../middlewares/authmiddlewares');
+const { registerVideoView, toggleVideoLike, checkLikeStatus } = require('../controllers/video.controller');
 
-
-router.get('/', verifyjwt, getallvideos);
+router.post('/', verifyjwt, getallvideos);
 router.post('/save', verifyjwt, upload.fields([
     {
         name: "videofile",
@@ -18,12 +18,15 @@ router.post('/save', verifyjwt, upload.fields([
 ]), publishAVideo);
 router.post('/delete', verifyjwt, deleteVideo);
 router.get('/publish', verifyjwt, togglePublishStatus);
-router.put('/update', verifyjwt, upload.single({
-    name: "thumbnail",
-    maxCount: 1
-}), updateVideo);
-router.get('/details', verifyjwt, getVideoById);
-
-
+router.put('/update', verifyjwt, upload.fields([
+    {
+        name: "thumbnail",
+        maxCount: 1
+    }
+]), updateVideo);
+router.post('/details', verifyjwt, getVideoById);
+router.post('/view', verifyjwt, registerVideoView);
+router.post('/like', verifyjwt, toggleVideoLike);
+router.get('/:videoId/like-status', verifyjwt, checkLikeStatus);
 
 module.exports = router;
